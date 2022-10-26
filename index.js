fff928d9fc5cc93f713ace424fdcc0628b4d14ba
mwendamwenda <benmwenda83@gmail.com>
2022-10-26 11:25:34 +0300

    completed code challenge2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('my main man')
+const johnBMI = 95 / 1.95 **2;
+
+const markBMI = 78/ 1.69 **2;
+//const markBMIisHigher = markBMI >johnBMI;
+
+if(markBMI >johnBMI){
+console.log(`marks BMI(${markBMI}) is is higher than that of john`)
+}else{
+console.log(`johns BMI (${johnBMI})is higher than that of john`)
+}
+console.log(johnBMI, markBMI);
